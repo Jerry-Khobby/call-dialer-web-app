5f2e2e71e5b4df7af41b59ce66a2eac4b320e27c
Jerry-Khobby <jerrymardeburg@gmail.com>
2025-11-07 12:20:07 +0000

    added some effect to the tab bar

--- client/src/app/components/organisms/tab-bar/tab-bar.ts
+++ client/src/app/components/organisms/tab-bar/tab-bar.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-tab-bar',
-  imports: [],
+  imports: [MatIconModule,RouterModule],
   templateUrl: './tab-bar.html',
   styleUrl: './tab-bar.css',
 })
