1fd7d2ed0ac5b2f94083cf2e18b2823a6e0d8a56
Jerry-Khobby <jerrymardeburg@gmail.com>
2025-11-07 11:44:08 +0000

    sharpening frontend of the keypad page

--- client/src/app/components/pages/keypad/keypad.page.ts
+++ client/src/app/components/pages/keypad/keypad.page.ts
@@ -2,12 +2,11 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { KeypadComponent } from '../../organisms/keypad/keypad.component';
 import { TabBar } from '../../organisms/tab-bar/tab-bar';
-
-
+import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-keypad-page',
   standalone: true,
-  imports: [CommonModule, KeypadComponent, TabBar],
+  imports: [CommonModule,MatIconModule, KeypadComponent, TabBar],
   templateUrl: './keypad.page.html'
 })
 export class KeypadPage {
