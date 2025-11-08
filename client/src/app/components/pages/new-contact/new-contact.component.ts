b3046238d86a3a5060769db0e492e13f3d29cde4
Jerry-Khobby <jerrymardeburg@gmail.com>
2025-11-08 11:53:44 +0000

    adding new contact page almost done

--- client/src/app/components/pages/new-contact/new-contact.component.ts
+++ client/src/app/components/pages/new-contact/new-contact.component.ts
@@ -1,17 +1,29 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-new-contact',
+  standalone: true,
+  imports: [CommonModule, MatIconModule],
   templateUrl: './new-contact.component.html',
   styleUrls: ['./new-contact.component.css']
 })
 export class NewContactComponent {
   phone: string = '';
 
-  constructor() {
+  constructor(private router: Router) {
     const nav = history.state;
-    if (nav && nav.phone) {
-      this.phone = nav.phone;
-    }
+    if (nav?.phone) this.phone = nav.phone;
+  }
+
+  cancel() {
+    this.router.navigate(['/keypad']);
+  }
+
+  done() {
+    alert('Contact Saved');
+    this.router.navigate(['/keypad']);
   }
 }
